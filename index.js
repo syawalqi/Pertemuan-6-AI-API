31eb66e1ea932c3640823e18dd1020f995e451a6
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-11 09:24:57 +0700

    konfigurasi index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+// Menjadikan folder "public" sebagai static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing default ke index.html di folder public
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+// Jalankan server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
